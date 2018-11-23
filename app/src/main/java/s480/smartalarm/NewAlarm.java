7e259a37600b2a76cf84b59d110e38b6c57353ee
David Dvorkin <ddvorki1@binghamton.edu>
2018-11-23 13:17:51 -0500

    only missing submit on new alarm form

--- app/src/main/java/s480/smartalarm/NewAlarm.java
+++ app/src/main/java/s480/smartalarm/NewAlarm.java
@@ -6,12 +6,18 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
 public class NewAlarm extends AppCompatActivity {
-
 
     public static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
@@ -26,20 +32,43 @@ public class NewAlarm extends AppCompatActivity {
             // Create a new instance of TimePickerDialog and return it
             return new TimePickerDialog(getActivity(), this, hour, minute,
                     DateFormat.is24HourFormat(getActivity()));
+
         }
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             // Do something with the time chosen by the user
+
+            final TextView text = getActivity().findViewById(R.id.inputTime);
+            if (minute >= 10) {
+                text.setText("" + hourOfDay + " : " + minute);
+            }
+            else{
+                text.setText("" + hourOfDay + " : 0" + minute);
+            }
         }
+
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_alarm);
 
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "timePicker");
+        final Button button = findViewById(R.id.SetTime);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+            }
+        });
+
+        final Button submit = findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+            }
+        });
 
 
     }
