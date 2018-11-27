f1326ae86858d829f7c62618e2a2c17e6604a1f6
David Dvorkin <ddvorki1@binghamton.edu>
2018-11-27 00:42:15 -0500

    Saves input correctly on app restart. Only actual alarm left to make

--- app/src/main/java/s480/smartalarm/MainMenu.java
+++ app/src/main/java/s480/smartalarm/MainMenu.java
@@ -1,15 +1,23 @@
 package s480.smartalarm;
 
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -22,6 +30,58 @@ public class MainMenu extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        SharedPreferences sharedpreferences = getSharedPreferences("Alarms", Context.MODE_PRIVATE);
+        //SharedPreferences.Editor editor = sharedpreferences.edit();
+        //editor.putString("key", "value");
+        //editor.commit();
+        //TextView dynamicTextView = new TextView(this);
+        //dynamicTextView.setLayoutParams(new Toolbar.LayoutParams(Toolbar.LayoutParams.WRAP_CONTENT, Toolbar.LayoutParams.WRAP_CONTENT));
+        //dynamicTextView.setText("Did I make text?");
+
+        RecyclerView alarms = findViewById(R.id.AlarmList);
+        Switch alarm1 = findViewById(R.id.switch1);
+        String holder = "";
+
+        if (!(sharedpreferences.getString("name", null) == null)){
+            holder += sharedpreferences.getString("name", null);
+        }
+
+        else {holder = "Default";}
+
+        if (!(sharedpreferences.getString("Monday", null) == null)){
+            holder += " " + sharedpreferences.getString("Monday", null);
+        }
+        if (!(sharedpreferences.getString("Tuesday", null) == null)) {
+            holder += " " + sharedpreferences.getString("Tuesday", null);
+        }
+        if (!(sharedpreferences.getString("Wednesday", null) == null)) {
+            holder += " " + sharedpreferences.getString("Wednesday", null);
+        }
+        if (!(sharedpreferences.getString("Thursday", null) == null)) {
+            holder += " " + sharedpreferences.getString("Thursday", null);
+        }
+        if (!(sharedpreferences.getString("Friday", null) == null)) {
+            holder += " " + sharedpreferences.getString("Friday", null);
+        }
+        if (!(sharedpreferences.getString("Saturday", null) == null)) {
+            holder += " " + sharedpreferences.getString("Saturday", null);
+        }
+        if (!(sharedpreferences.getString("Sunday", null) == null)) {
+            holder += " " + sharedpreferences.getString("Sunday", null);
+        }
+        if (!(sharedpreferences.getString("None", null) == null)) {
+            holder += sharedpreferences.getString("None", null);
+        }
+        if (!(sharedpreferences.getString("Shaker", null) == null)) {
+            holder += sharedpreferences.getString("Shaker", null);
+        }
+        if (!(sharedpreferences.getString("Rotater", null) == null)) {
+            holder += sharedpreferences.getString("Rotater", null);
+        }
+
+        alarm1.setText(holder);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         final Intent intent = new Intent(this, NewAlarm.class);
@@ -31,30 +91,75 @@ public class MainMenu extends AppCompatActivity {
             public void onClick(View view) {
                 /**Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();**/
-                startActivity(intent);
+                //startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        SharedPreferences sharedpreferences = getSharedPreferences("Alarms", Context.MODE_PRIVATE);
+        Switch alarm1 = findViewById(R.id.switch1);
+        String holder = "";
+        // Check which request we're responding to
+        if (requestCode == 1) {
+                if (!(sharedpreferences.getString("name", null) == null)){
+                    holder += sharedpreferences.getString("name", null);
+                }
+                if (!(sharedpreferences.getString("Monday", null) == null)){
+                    holder += " " + sharedpreferences.getString("Monday", null);
+                }
+                if (!(sharedpreferences.getString("Tuesday", null) == null)) {
+                    holder += " " + sharedpreferences.getString("Tuesday", null);
+                }
+                if (!(sharedpreferences.getString("Wednesday", null) == null)) {
+                    holder += " " + sharedpreferences.getString("Wednesday", null);
+                }
+                if (!(sharedpreferences.getString("Thursday", null) == null)) {
+                    holder += " " + sharedpreferences.getString("Thursday", null);
+                }
+                if (!(sharedpreferences.getString("Friday", null) == null)) {
+                    holder += " " + sharedpreferences.getString("Friday", null);
+                }
+                if (!(sharedpreferences.getString("Saturday", null) == null)) {
+                    holder += " " + sharedpreferences.getString("Saturday", null);
+                }
+                if (!(sharedpreferences.getString("Sunday", null) == null)) {
+                    holder += " " + sharedpreferences.getString("Sunday", null);
+                }
+                if (!(sharedpreferences.getString("None", null) == null)) {
+                    holder += sharedpreferences.getString("None", null);
+                }
+                if (!(sharedpreferences.getString("Shaker", null) == null)) {
+                    holder += sharedpreferences.getString("Shaker", null);
+                }
+                if (!(sharedpreferences.getString("Rotater", null) == null)) {
+                    holder += sharedpreferences.getString("Rotater", null);
+                }
+
+                alarm1.setText(holder);
+           // }
+        }
     }
+    private void scheduleAlarm(int dayOfWeek, int hour, int minute) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Calendar calendar = Calendar.getInstance();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.DAY_OF_WEEK, dayOfWeek);
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+        // Check we aren't setting it in the past which would trigger it to fire instantly
+        if(calendar.getTimeInMillis() < System.currentTimeMillis()) {
+            calendar.add(Calendar.DAY_OF_YEAR, 7);
         }
 
-        return super.onOptionsItemSelected(item);
+        // Set this to whatever you were planning to do at the given time
+        //PendingIntent yourIntent;
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY * 7, /**yourIntent**/);
     }
 }
--- app/src/main/java/s480/smartalarm/NewAlarm.java
+++ app/src/main/java/s480/smartalarm/NewAlarm.java
@@ -2,13 +2,17 @@ package s480.smartalarm;
 
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -40,10 +44,10 @@ public class NewAlarm extends AppCompatActivity {
 
             final TextView text = getActivity().findViewById(R.id.inputTime);
             if (minute >= 10) {
-                text.setText("" + hourOfDay + " : " + minute);
+                text.setText("" + hourOfDay + ":" + minute);
             }
             else{
-                text.setText("" + hourOfDay + " : 0" + minute);
+                text.setText("" + hourOfDay + ":0" + minute);
             }
         }
 
@@ -65,6 +69,83 @@ public class NewAlarm extends AppCompatActivity {
         final Button submit = findViewById(R.id.submitButton);
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                final TextView text = findViewById(R.id.inputTime);
+                final CheckBox Mon = findViewById(R.id.Mon);
+                final CheckBox Tues = findViewById(R.id.Tues);
+                final CheckBox Wed = findViewById(R.id.Wed);
+                final CheckBox Thur = findViewById(R.id.Thur);
+                final CheckBox Fri = findViewById(R.id.Fri);
+                final CheckBox Sat = findViewById(R.id.Sat);
+                final CheckBox Sun = findViewById(R.id.Sun);
+                final RadioButton None = findViewById(R.id.None);
+                final RadioButton Shaker = findViewById(R.id.shaker);
+                final RadioButton Rotater = findViewById(R.id.rotater);
+                SharedPreferences sharedpreferences = getSharedPreferences("Alarms", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                editor.putString("name", text.getText().toString());
+                if (Mon.isChecked()) {
+                    editor.putString("Monday", Mon.getText().toString());
+                }
+                else{
+                    editor.putString("Monday", null);
+                }
+                if (Tues.isChecked()) {
+                    editor.putString("Tuesday", Tues.getText().toString());
+                }
+                else{
+                    editor.putString("Tuesday", null);
+                }
+                if (Wed.isChecked()) {
+                    editor.putString("Wednesday", Wed.getText().toString());
+                }
+                else{
+                    editor.putString("Wednesday", null);
+                }
+                if (Thur.isChecked()) {
+                    editor.putString("Thursday", Thur.getText().toString());
+                }
+                else{
+                    editor.putString("Thursday", null);
+                }
+                if (Fri.isChecked()) {
+                    editor.putString("Friday", Fri.getText().toString());
+                }
+                else{
+                    editor.putString("Friday", null);
+                }
+                if (Sat.isChecked()) {
+                    editor.putString("Saturday", Sat.getText().toString());
+                }
+                else{
+                    editor.putString("Saturday", null);
+                }
+                if (Sun.isChecked()) {
+                    editor.putString("Sunday", Sun.getText().toString());
+                }
+                else{
+                    editor.putString("Sunday", null);
+                }
+                if (None.isChecked()){
+                    editor.putString("None", "\n Alarm off method: None");
+                }
+                else{
+                    editor.putString("None", null);
+                }
+                if (Shaker.isChecked()){
+                    editor.putString("Shaker", "\n Alarm off method: Shake");
+                }
+                else{
+                    editor.putString("Shaker", null);
+                }
+                if (Rotater.isChecked()){
+                    editor.putString("Rotater", "\n Alarm off method: Rotate");
+                }
+                else{
+                    editor.putString("Rotater", null);
+                }
+
+                editor.commit();
                 finish();
             }
         });
